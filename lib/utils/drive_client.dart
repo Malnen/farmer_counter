15188a5348bfdaf38232772df27617a4df20fab5
malne <malnen13@gmail.com>
2025-08-28 18:53:52 +0200

    fixed google sign in issues

--- lib/utils/drive_client.dart
+++ lib/utils/drive_client.dart
@@ -2,13 +2,17 @@ import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:googleapis/drive/v3.dart' as drive;
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum DriveAuthState { disconnected, connected }
 
 class DriveClient {
+  static const String _kAutoSignInKey = 'drive.autoSignInEnabled';
+
   final ValueNotifier<DriveAuthState> authState;
 
   bool _initialized;
+  bool _autoSignInEnabled = false;
   GoogleSignInAccount? _account;
   Map<String, String>? _headers;
 
@@ -24,10 +28,10 @@ class DriveClient {
     await _ensureInit();
     try {
       final GoogleSignInAccount account = await GoogleSignIn.instance.authenticate(
-        scopeHint: <String>[drive.DriveApi.driveScope],
+        scopeHint: <String>[drive.DriveApi.driveFileScope],
       );
       final Map<String, String>? headers = await account.authorizationClient.authorizationHeaders(
-        <String>[drive.DriveApi.driveScope],
+        <String>[drive.DriveApi.driveFileScope],
         promptIfNecessary: true,
       );
       if (headers == null) {
@@ -37,6 +41,10 @@ class DriveClient {
       _account = account;
       _headers = headers;
       authState.value = DriveAuthState.connected;
+      _autoSignInEnabled = true;
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setBool(_kAutoSignInKey, true);
+
       return true;
     } catch (_) {
       return false;
@@ -49,14 +57,22 @@ class DriveClient {
     _account = null;
     _headers = null;
     authState.value = DriveAuthState.disconnected;
+    _autoSignInEnabled = false;
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.setBool(_kAutoSignInKey, false);
   }
 
   Future<void> loadState() async {
     await _ensureInit();
+    if (!_autoSignInEnabled) {
+      authState.value = DriveAuthState.disconnected;
+      return;
+    }
+
     _account = await GoogleSignIn.instance.attemptLightweightAuthentication();
     if (_account != null) {
       _headers = await _account!.authorizationClient.authorizationHeaders(
-        <String>[drive.DriveApi.driveScope],
+        <String>[drive.DriveApi.driveFileScope],
         promptIfNecessary: false,
       );
       if (_headers != null) {
@@ -70,6 +86,8 @@ class DriveClient {
 
   Future<void> _ensureInit() async {
     if (!_initialized) {
+      final SharedPreferences preferences = await SharedPreferences.getInstance();
+      _autoSignInEnabled = preferences.getBool(_kAutoSignInKey) ?? false;
       await GoogleSignIn.instance.initialize(
         clientId: '156428963186-onde9hov3jkpf2sjebkoumnvm218t4v9.apps.googleusercontent.com',
         serverClientId: '156428963186-91jv4r0prmp50qu7japfos3v6mobg918.apps.googleusercontent.com',
